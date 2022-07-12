5cf39819e97d846ac4539c84d0fcfe94b44850e2
azotamiota <azotamiota@gmail.com>
2022-07-12 11:50:11 +0100

    commit changes and gitignore

--- numberFunctions.js
+++ numberFunctions.js
@@ -2,6 +2,8 @@ const getLargest = (a, b, c) => {
     return Math.max(a,b,c)
 }
 
-console.log(getLargest(1,2,3))
+const getSmallest = (a, b, c) => {
+    return Math.min(a,b,c)
+}
 
 module.exports = getLargest
