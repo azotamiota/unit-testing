cd9b481500442603cc64aaeee1afd18d461ee564
azotamiota <azotamiota@gmail.com>
2022-07-12 11:03:58 +0100

    REfractor tests to use each

--- tests/getLargest.test.js
+++ tests/getLargest.test.js
@@ -1,3 +1,4 @@
+const each = require('jest-each')
 const getLargest = require('../getLargest')
 
 describe('getLargest', () => {
@@ -7,10 +8,22 @@ describe('getLargest', () => {
     })
     
     
-    test('It returns the largest number', () => {
-        const result = getLargest(1,2,3);
-        expect(result).toEqual(3);
-    }
-    )
+    describe("It handles normal inputs successfully", () => {
+
+        each([[[1,2,3], 3],
+        [[9,8,7], 9],
+        [[12,36,19], 36]])
+        .test("", (arr, expected) => {
+            expect(getLargest(arr).toEqual(expected))
+        })
+        // test("", () => {
+        //     const result = getLargest(1,2,3);
+        //     const result2 = getLargest(-1,-2,-3);
+        //     const result3 = getLargest(0.4, 99, 2323);
+        //     expect(result).toEqual(3);
+        //     expect(result2).toEqual(-1);
+        //     expect(result3).toEqual(2323);
+        // })
+    })
 })
 
